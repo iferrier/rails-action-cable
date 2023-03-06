f97984fbefccd3a4b275d254142459edd7d2b2b2
Isabelle Ferrier <ferrierisabelle@gmail.com>
2023-03-06 10:59:29 +0000

    create rails app with chat messenger feature for batch 1158 lisbon

---
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -0,0 +1,31 @@
+import { Controller } from "@hotwired/stimulus"
+import { createConsumer } from "@rails/actioncable";
+
+// Connects to data-controller="chatroom-subscription"
+export default class extends Controller {
+  static targets = ["messages"]
+  static values = {
+    chatroomId: Number
+  }
+
+  connect() {
+    this.channel = createConsumer().subscriptions.create(
+      { channel: "ChatroomChannel", id: this.chatroomIdValue },
+      { received: (data) => this.#insertMessageAndScrollDown(data) }
+    );
+  }
+
+  #insertMessageAndScrollDown(data) {
+    this.messagesTarget.insertAdjacentHTML("beforeend", data)
+    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
+  }
+
+  resetForm(e) {
+    e.target.reset()
+  }
+
+  disconnect() {
+    console.log("Unsubscribed from the chatroom")
+    this.channel.unsubscribe()
+  }
+}
